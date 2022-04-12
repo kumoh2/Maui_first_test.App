651611ea652c2a76a8932032e2bbf89183262a63
kumoh2 <kumoh2@icloud.com>
2022-04-12 11:30:16 +0900

    Feat: Exit function, login xaml add
    종료 버튼 함수 할당 및
    로그인 화면 xaml 추가

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace Maui_first_test;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Login_Button_Click(object sender, EventArgs e)
+	{
+	}
+	private void Exit_Button_Click(object sender, EventArgs e)
+	{
+		Environment.Exit(0);
+	}
+}
